488b52f5c53159daaf6798d113474625a600559a
torpedo-t <55634516+torpedo-t@users.noreply.github.com>
2022-06-14 15:10:18 -0500

    resources associations

--- app/models/rated_recipe.rb
+++ app/models/rated_recipe.rb
@@ -1,2 +1,4 @@
 class RatedRecipe < ApplicationRecord
+    belongs_to :user
+    belongs_to :recipe
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
     has_many :recipes
+    has_many :ratings
+    has_many :rated_recipes, through :ratings
 end
